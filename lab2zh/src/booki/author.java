3b30c3df401e998e5c6e27bcfe63a97b54d116ab
zeron <zhhaiesechit@gmail.com>
2014-10-17 14:40:50 +0800

    init my lab2zh reposi

---
+++ lab2zh/src/booki/author.java
@@ -0,0 +1,38 @@
+package booki;
+
+public class author {
+	private String AuthorID, Name, Country,Age;
+	public String getAuthorID() {
+		return AuthorID;
+	}
+
+	public void setAuthorID(String AuthorID) {
+		this.AuthorID = AuthorID;
+	}
+
+	public String getName() {
+		return Name;}
+	public String getAge() {
+		return Age;
+	}
+
+	public void setAge(String Age) {
+		this.Age = Age;
+	}
+
+
+	
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public String getCountry() {
+		return Country;
+	}
+
+	public void setCountry(String Country) {
+		this.Country = Country;
+	}
+
+}
